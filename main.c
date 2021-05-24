a2407d08f91831c24ea69321ea94ae6f805a2607
Riya Desai <77716714+RiyaDesai09@users.noreply.github.com>
2021-05-24 10:25:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,91 +1,138 @@
+#include <iostream>
 #include <stdio.h>
-#include <conio.h>
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-int checkwin();
-void board();
-int main()
-{
-int player = 1, i, choice;
-char mark;
-do
-{
-board();
-player = (player % 2) ? 1 : 2;
-printf("Player %d, enter a number: ", player);
-scanf("%d", &choice);
-mark = (player == 1) ? 'X' : 'O';
-if (choice == 1 && square[1] == '1')
-square[1] = mark;
-else if (choice == 2 && square[2] == '2')
-square[2] = mark;
-else if (choice == 3 && square[3] == '3')
-square[3] = mark;
-else if (choice == 4 && square[4] == '4')
-square[4] = mark;
-else if (choice == 5 && square[5] == '5')
-square[5] = mark;
-else if (choice == 6 && square[6] == '6')
-square[6] = mark;
-else if (choice == 7 && square[7] == '7')
-square[7] = mark;
-else if (choice == 8 && square[8] == '8')
-square[8] = mark;
-else if (choice == 9 && square[9] == '9')
-square[9] = mark;
-else
-{
-printf("Invalid move ");
-player--;
-getch();
-}
-i = checkwin();
-player++;
-}while (i == - 1);
-board();
-if (i == 1)
-printf("==>\aPlayer %d win ", --player);
-else
-printf("==>\aGame draw");
-getch();
-return 0;
-}
+using namespace std;
+char square[9] = {'0','1','2','3','4','5','6','7','8'};
 int checkwin()
 {
-if (square[1] == square[2] && square[2] == square[3])
-return 1;
-else if (square[4] == square[5] && square[5] == square[6])
-return 1;
-else if (square[7] == square[8] && square[8] == square[9])
-return 1;
-else if (square[1] == square[4] && square[4] == square[7])
-return 1;
-else if (square[2] == square[5] && square[5] == square[8])
-return 1;
-else if (square[3] == square[6] && square[6] == square[9])
-return 1;
-else if (square[1] == square[5] && square[5] == square[9])
-return 1;
-else if (square[3] == square[5] && square[5] == square[7])
-return 1;
-else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7]
-!= '7' && square[8] != '8' && square[9] != '9')
-return 0;
+        if (square[0] == square [1]  && square[1] == square[2] )
+        {   if ( square [0] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
+        else
+        if (square[3] == square [4]  && square[4] == square[5] )
+            {   if ( square [3] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
+        else
+        if (square[6] == square [7]  && square[7] == square[8] )
+            {   if ( square [6] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
+        else
+        if (square[0] == square [3]  && square[3] == square[6] )
+            {   if ( square [0] == 'X' )
+            return 1;
+            else
+           return 2;
+
+        }
+        else
+        if (square[1] == square [4]  && square[4] == square[7] )
+            {   if ( square [1] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
+    else
+        if (square[2] == square [5]  && square[5] == square[8] )
+            {   if ( square [2] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
 else
-return - 1;
+        if (square[0] == square [4]  && square[4] == square[8] )
+            {   if ( square [0] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
+    else
+        if (square[2] == square [4]  && square[4] == square[6] )
+            {   if ( square [2] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
+    else
+        if (square[0] == square [3]  && square[3] == square[6] )
+            {   if ( square [0] == 'X' )
+            return 1;
+            else
+            return 2;
+        }
+    else
+        return 0;
 }
-void board()
+ 
+void mark(int player, int box)
 {
-system("cls");
-printf("\n\n\t Tic Tac Toe \n\n");
-printf("Player 1 (X) - Player 2 (O)\n\n\n");
-printf(" | | \n");
-printf(" %c | %c | %c \n", square[1], square[2], square[3]);
-printf("_____|_____|_____\n");
-printf(" | | \n");
-printf(" %c | %c | %c \n", square[4], square[5], square[6]);
-printf("_____|_____|_____\n");
-printf(" | | \n");
-printf(" %c | %c | %c \n", square[7], square[8], square[9]);
-printf(" | | \n\n");
+    if (player == 1 )
+    {
+ 
+        square[box] = 'X';
+    }
+    else
+        square[box] = 'O';
+}
+ 
+void display()
+{
+        for(int i=0;i<9;i++)
+        {
+            cout<< square[i] << "\t" ;
+                if (i == 2 || i== 5 || i==8)
+                    cout<<"\n";
+}
+}
+int main()
+{
+        int player1 = 1, player2 =2 ;
+        int box, result = 0, flag = 0;
+        for(int i=1;i<5;i++)
+    {
+        cout<< "\n Player " << player1 << "Enter the Box";
+        cin>> box;
+        mark( player1, box);
+        display();
+        result =checkwin();
+        if (result == 1 )
+        {   cout<<"\n Congratualtions! player " << player1 << " has Won ";
+            flag = 1;
+            break;
+        }
+        else
+        if (result == 2 )
+        {   cout<<"\n Congratualtions! player " << player2 << " has Won ";
+            flag = 1;
+            break;
+        }
+        cout<< "\n Player " << player2 << "Enter the Box";
+        cin>> box;
+        mark ( player2, box);
+        display();
+        result =checkwin();
+        if (result == 1 )
+        {   cout<<"\n Congratualtions! player " << player1 << " has Won ";
+            flag = 1;
+            break;
+        }
+        else
+        if (result == 2 )
+        {   cout<<"\n Congratualtions! player " << player2 << " has Won ";
+            flag = 1;
+            break;
+        }
+}
+        if (flag == 0 )
+
+       cout<<" \n Sorry, The game is a draw ";
+
+       return 0;
 }
